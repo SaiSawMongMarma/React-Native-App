f221718a4062e1117c75c6cd202c6d2c10025a5c
Sai Saw Mong <saisawmong34@outlook.com>
2026-04-11 20:45:05 +0600

    ok

---
+++ src/features/Focus.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import { TextInput } from 'react-native-paper';
+
+import { colors } from '../utils/colors';
+
+export const Focus = () => (
+    <View style = {styles.container}>
+        <Text style = {styles.text}>Focus Features</Text>
+    </View>
+)
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+    
+
+    },
+
+    text: {
+        color: colors.white,
+    }
+});
